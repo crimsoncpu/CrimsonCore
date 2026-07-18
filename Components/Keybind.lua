6febabb92d7670572165a0575ddb97d37335e1e9
Crimson <realbakonblox@gmail.com>
2026-07-18 17:52:29 -0400

    Update Keybind.lua

--- Components/Keybind.lua
+++ Components/Keybind.lua
@@ -5,50 +5,144 @@ local Keybind = {}
 local UserInputService = game:GetService("UserInputService")
 
 
-function Keybind:Create(Parent, config, Theme, Utility)
+
+function Keybind:Create(parent, config, Theme, Utility)
 
 	config = config or {}
 
 
-	local CurrentKey = config.Default or Enum.KeyCode.RightShift
+	local CurrentKey =
+		config.Default or Enum.KeyCode.RightShift
+
 
 	local Listening = false
 
 
 
+	local Holder = Instance.new("Frame")
+
+	Holder.Name = config.Text or "Keybind"
+
+	Holder.Size =
+		UDim2.new(
+			1,
+			-10,
+			0,
+			45
+		)
+
+
+	Holder.BackgroundColor3 =
+		Theme.Panel
+
+
+	Holder.Parent = parent
+
+
+
+	Utility:Corner(
+		Holder,
+		12
+	)
+
+
+	Utility:Stroke(
+		Holder,
+		Theme.Crimson,
+		1.5
+	)
+
+
+
+	local Label = Instance.new("TextLabel")
+
+	Label.Size =
+		UDim2.new(
+			.6,
+			0,
+			1,
+			0
+		)
+
+
+	Label.Position =
+		UDim2.fromOffset(
+			15,
+			0
+		)
+
+
+	Label.BackgroundTransparency = 1
+
+
+	Label.Text =
+		config.Text or "Keybind"
+
+
+	Label.TextColor3 =
+		Theme.Text
+
+
+	Label.Font =
+		Enum.Font.BuilderSansBold
+
+
+	Label.TextSize = 15
+
+
+	Label.TextXAlignment =
+		Enum.TextXAlignment.Left
+
+
+	Label.Parent = Holder
+
+
+
 	local Button = Instance.new("TextButton")
 
-	Button.Size = UDim2.new(1,-10,0,38)
+	Button.Size =
+		UDim2.fromOffset(
+			100,
+			30
+		)
 
-	Button.BackgroundColor3 = Theme.Secondary
 
-	Button.TextColor3 = Theme.Text
+	Button.Position =
+		UDim2.new(
+			1,
+			-110,
+			.5,
+			-15
+		)
 
-	Button.Font = Enum.Font.BuilderSansBold
+
+	Button.BackgroundColor3 =
+		Theme.Crimson
+
+
+	Button.Text =
+		CurrentKey.Name
+
+
+	Button.TextColor3 =
+		Theme.Text
+
+
+	Button.Font =
+		Enum.Font.BuilderSansBold
+
 
 	Button.TextSize = 14
 
-	Button.AutoButtonColor = false
 
-	Button.Parent = Parent
-
-
-
-	Utility:Corner(Button,10)
+	Button.Parent = Holder
 
 
 
-	local function Update()
-
-		Button.Text =
-			(config.Name or "Keybind")..
-			": "..CurrentKey.Name
-
-	end
-
-
-
-	Update()
+	Utility:Corner(
+		Button,
+		8
+	)
 
 
 
@@ -56,7 +150,7 @@ function Keybind:Create(Parent, config, Theme, Utility)
 
 		Listening = true
 
-		Button.Text = "Press a key..."
+		Button.Text = "Press Key"
 
 	end)
 
@@ -69,25 +163,42 @@ function Keybind:Create(Parent, config, Theme, Utility)
 		end
 
 
+
 		if Listening then
+
 
 			if input.KeyCode ~= Enum.KeyCode.Unknown then
 
-				CurrentKey = input.KeyCode
+
+				CurrentKey =
+					input.KeyCode
+
+
+				Button.Text =
+					CurrentKey.Name
+
 
 				Listening = false
 
-				Update()
 
 			end
 
-		elseif input.KeyCode == CurrentKey then
+
+			return
+
+		end
+
+
+
+		if input.KeyCode == CurrentKey then
+
 
 			if config.Callback then
 
 				config.Callback()
 
 			end
+
 
 		end
 
@@ -101,11 +212,21 @@ function Keybind:Create(Parent, config, Theme, Utility)
 
 			return CurrentKey
 
+		end,
+
+
+		Set = function(_,key)
+
+			CurrentKey = key
+
+			Button.Text = key.Name
+
 		end
 
 	}
 
 end
+
 
 
 return Keybind
