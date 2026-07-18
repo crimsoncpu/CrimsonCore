7c26e16fd6af8813073477047f018cc72e4445b3
Crimson <realbakonblox@gmail.com>
2026-07-18 17:34:02 -0400

    Update Config.lua

--- Core/Config.lua
+++ Core/Config.lua
@@ -1,21 +1,89 @@
---// CrimsonCore Config v1.0
+--// CrimsonCore Config System v2.0
+--// Premium Hybrid Edition
 
 local Config = {}
 
-Config.Name = "CrimsonCore"
 
-Config.Version = "1.0.0"
+-- Window
 
-Config.AnimationSpeed = 0.25
+Config.Window = {
 
-Config.DefaultSize = {
 	Width = 760,
-	Height = 500
+
+	Height = 500,
+
+	CornerRadius = 18,
+
+	Draggable = true,
+
+	Resizable = true
+
 }
 
-Config.MobileSize = {
-	Width = 500,
-	Height = 450
+
+
+-- Animation
+
+Config.Animation = {
+
+	OpenTime = .45,
+
+	CloseTime = .3,
+
+	HoverTime = .15,
+
+	SnapTime = .45
+
 }
+
+
+
+-- Floating Button
+
+Config.Floating = {
+
+	Enabled = true,
+
+	Size = 62,
+
+	Snap = true
+
+}
+
+
+
+-- Notifications
+
+Config.Notification = {
+
+	Enabled = true,
+
+	Duration = 4,
+
+	Position = "BottomRight"
+
+}
+
+
+
+-- Components
+
+Config.Components = {
+
+	Buttons = true,
+
+	Toggles = true,
+
+	Sliders = true
+
+}
+
+
+
+-- Debug
+
+Config.Debug = false
+
+
 
 return Config
