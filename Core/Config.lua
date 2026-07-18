05e4f47161744b3c95369301693c51d7da337387
Crimson <realbakonblox@gmail.com>
2026-07-18 14:39:04 -0400

    Create Config.lua

---
+++ Core/Config.lua
@@ -0,0 +1,21 @@
+--// CrimsonCore Config v1.0
+
+local Config = {}
+
+Config.Name = "CrimsonCore"
+
+Config.Version = "1.0.0"
+
+Config.AnimationSpeed = 0.25
+
+Config.DefaultSize = {
+	Width = 760,
+	Height = 500
+}
+
+Config.MobileSize = {
+	Width = 500,
+	Height = 450
+}
+
+return Config
