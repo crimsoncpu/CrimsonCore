abee5da6f8caa4bcec0377f8e77aeef4c1cf1ff4
Crimson <realbakonblox@gmail.com>
2026-07-18 17:52:53 -0400

    Update Label.lua

--- Components/Label.lua
+++ Components/Label.lua
@@ -3,45 +3,102 @@
 local Label = {}
 
 
-function Label:Create(Parent, config, Theme, Utility)
+function Label:Create(parent, config, Theme, Utility)
 
 	config = config or {}
 
 
+	local Holder = Instance.new("Frame")
+
+	Holder.Name = config.Text or "Label"
+
+	Holder.Size =
+		UDim2.new(
+			1,
+			-10,
+			0,
+			40
+		)
+
+
+	Holder.BackgroundColor3 =
+		Theme.Panel
+
+
+	Holder.Parent = parent
+
+
+
+	Utility:Corner(
+		Holder,
+		10
+	)
+
+
+	Utility:Stroke(
+		Holder,
+		Theme.Crimson,
+		1
+	)
+
+
+
 	local Text = Instance.new("TextLabel")
 
-	Text.Size = UDim2.new(1,-10,0,32)
+	Text.Size =
+		UDim2.new(
+			1,
+			-20,
+			1,
+			0
+		)
 
-	Text.BackgroundColor3 = Theme.Secondary
 
-	Text.BackgroundTransparency = 0
+	Text.Position =
+		UDim2.fromOffset(
+			10,
+			0
+		)
 
-	Text.TextColor3 = Theme.Text
 
-	Text.Font = Enum.Font.BuilderSansBold
+	Text.BackgroundTransparency = 1
+
+
+	Text.Text =
+		config.Text or "Label"
+
+
+	Text.TextColor3 =
+		Theme.SubText
+
+
+	Text.Font =
+		Enum.Font.BuilderSans
+
 
 	Text.TextSize = 14
 
-	Text.TextXAlignment = Enum.TextXAlignment.Left
 
-	Text.Text = config.Text or "Label"
-
-	Text.Parent = Parent
+	Text.TextXAlignment =
+		Enum.TextXAlignment.Left
 
 
-
-	Utility:Corner(Text,10)
+	Text.Parent = Holder
 
 
 
 	return {
 
-		Object = Text,
-
-
-		Set = function(value)
+		Set = function(_,value)
 
 			Text.Text = value
+
+		end,
+
+
+		Get = function()
+
+			return Text.Text
 
 		end
 
