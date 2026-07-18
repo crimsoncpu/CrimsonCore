d1ed6acd1a7c3ac111430b026f8c65b6cb1f6a06
Crimson <realbakonblox@gmail.com>
2026-07-18 14:37:51 -0400

    Create Theme.lua

---
+++ Core/Theme.lua
@@ -0,0 +1,14 @@
+--// CrimsonCore Theme v1.0
+
+local Theme = {}
+
+Theme.Background = Color3.fromRGB(10,10,15)
+Theme.Panel = Color3.fromRGB(20,20,30)
+
+Theme.Crimson = Color3.fromRGB(220,20,60)
+Theme.Secondary = Color3.fromRGB(45,45,60)
+
+Theme.Text = Color3.fromRGB(255,255,255)
+Theme.Muted = Color3.fromRGB(160,160,170)
+
+return Theme
